ba8e1a634c04ff7885d2573e618788b2a974c6df
Matthew Zimmer <M16r01@gmail.com>
2016-11-06 22:38:39 -0500

    implemented a full network which computes the sum of the output from Add(Add(x, y), y) --> (x+y) + y

--- nn.py
+++ nn.py
@@ -21,3 +21,29 @@ sorted_neurons = topological_sort(feed_dict)
 output = forward_pass(f, sorted_neurons)
 
 print("{} + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], output))
+
+
+# Cheat version to built a network off of an existing network's output.
+p = Input()
+r = Add(p, y)
+
+feed_dict = {p: output, x: feed_dict[x], y: feed_dict[y]}
+
+sorted_neurons = topological_sort(feed_dict)
+output = forward_pass(r, sorted_neurons)
+
+print("({} + {}) + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], feed_dict[y], output))
+
+
+# Full network implementation to Add (x + y) + y
+
+x, y = Input(), Input()
+
+f = Add(Add(x, y), y)
+
+feed_dict = {x: 10, y: 5}
+
+sorted_neurons = topological_sort(feed_dict)
+output = forward_pass(f, sorted_neurons)
+
+print("({} + {}) + {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], feed_dict[y], output))
