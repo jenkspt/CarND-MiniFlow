7de1b4bea5b9fb2e9a0553dad2ca375834ac5ba9
Matthew Zimmer <M16r01@gmail.com>
2016-11-07 00:02:06 -0500

    proper comment

--- bonus_nn.py
+++ bonus_nn.py
@@ -39,5 +39,5 @@ feed_dict = {x: 4, y: 5, z: 10}
 graph = topological_sort(feed_dict)
 output = forward_pass(f, graph)
 
-# should output 19
+# should output 200
 print("{} * {} * {} = {} (according to miniflow)".format(feed_dict[x], feed_dict[y], feed_dict[z], output))
